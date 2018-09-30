6a06678cdbdecfb83f33c530b058533f15df7834
EKibet <kibetedgar@gmail.com>
2018-09-30 11:38:13 +0300

    Refactors templates usage

--- gallery/views.py
+++ gallery/views.py
@@ -16,22 +16,22 @@ def search_results(request):
         searched_image = Image.search_by_category(search_term)
         message = f"{search_term}"
 
-        return render(request, 'search.html',{"message":message,"search_results": searched_image})
+        return render(request, 'index.html',{"message":message,"all_images": searched_image})
 
     else:
         message = "You haven't searched for any term"
-        return render(request, 'search.html',{"message":message})
+        return render(request, 'index.html',{"message":message})
 
 def get_category(request,category):
     category_results = Category.objects.all()
     location_results = Location.objects.all()
     category_result = Image.objects.filter(image_category__cat_name = category)
-    return render(request,'search.html',{'search_results':category_result,'category_results':category_results,'location_results':location_results})
+    return render(request,'index.html',{'all_images':category_result,'category_results':category_results,'location_results':location_results})
 
 def get_location(request,location):
     category_results = Category.objects.all()
     location_results = Location.objects.all()
     location_result = Image.objects.filter(image_location__location_name= location)
-    return render(request,'search.html',{'search_results':location_result,'category_results':category_results,'location_results':location_results})
+    return render(request,'index.html',{'all_images':location_result,'category_results':category_results,'location_results':location_results})
 
     
