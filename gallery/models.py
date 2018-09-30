647152f8803d2c9e8b49b904c374532b56f3d48a
EKibet <kibetedgar@gmail.com>
2018-09-30 11:19:14 +0300

    Updates gallery-Image methods

--- gallery/models.py
+++ gallery/models.py
@@ -39,3 +39,34 @@ class Image(models.Model):
     def search_by_category(cls,search_term):
         search_result = cls.objects.filter(image_category__cat_name__icontains=search_term)
         return search_result
+
+    @classmethod
+    def get_image_by_id(cls,incoming_id):
+        image_result = cls.objects.get(id=incoming_id)
+        return image_result
+
+    def save_image(self):
+        self.save()
+    def delete_image(self):
+        self.delete()
+
+        
+    @classmethod
+    def retrieve_all(cls):
+        all_objects = Image.objects.all()
+        for item in all_objects:
+            return item;
+
+
+    @classmethod
+    def update_image(cls,current_value,new_value):
+        fetched_object = Image.objects.filter(image_name=current_value).update(image_name=new_value)
+        return fetched_object
+
+    @classmethod
+    def filter_by_location(cls,location):
+        filtered_result = cls.objects.filter(image_location__location_name__icontains=location)
+        return filtered_result
+
+
+
