e0a87d895393b79ec5b8abc7016e757313dec81b
EKibet <kibetedgar@gmail.com>
2018-09-30 11:16:59 +0300

    Adds route to process getlocation and get by category fn

--- gallery/urls.py
+++ gallery/urls.py
@@ -6,6 +6,9 @@ from django.conf.urls.static import static
 urlpatterns=[
     url(r'^$',views.welcome,name='home-page'),
     url(r'^search/',views.search_results, name='search_results'),
-
+    url(r'^category/(\w+)', views.get_category,name='get_category'),
+    url(r'^location/(\w+)', views.get_location,name='get_location'),
 
 ]
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)  
