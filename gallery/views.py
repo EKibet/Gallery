e29aae6751caaf3dd08a2f1c595f1724f0e0e3c8
EKibet <kibetedgar@gmail.com>
2018-09-30 11:14:22 +0300

    Adds image display fn in views

--- gallery/views.py
+++ gallery/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from .models import Image,Category,Location
 
 # Create your views here.
 def welcome(request):
-    return render(request,'index.html')
+    all_images = Image.objects.all()
+    category_results = Category.objects.all()
+    location_results = Location.objects.all()
+    return render(request,'index.html', {'all_images':all_images,'location_results':location_results,'category_results':category_results})
+
