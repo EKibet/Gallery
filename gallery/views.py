716a77fcb07baed11bc16582c7b210fc6b92751b
EKibet <kibetedgar@gmail.com>
2018-09-30 11:14:48 +0300

    Adds search fn in views

--- gallery/views.py
+++ gallery/views.py
@@ -8,3 +8,16 @@ def welcome(request):
     location_results = Location.objects.all()
     return render(request,'index.html', {'all_images':all_images,'location_results':location_results,'category_results':category_results})
 
+
+def search_results(request):
+    
+    if 'searchItem' in request.GET and request.GET["searchItem"]:
+        search_term = request.GET.get("searchItem")
+        searched_image = Image.search_by_category(search_term)
+        message = f"{search_term}"
+
+        return render(request, 'search.html',{"message":message,"search_results": searched_image})
+
+    else:
+        message = "You haven't searched for any term"
+        return render(request, 'search.html',{"message":message})
